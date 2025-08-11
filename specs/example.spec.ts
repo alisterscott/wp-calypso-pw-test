5edc2c4bbcb71e9cc203973d6cb511b80e5498e3
Alister Scott <alister.scott@gmail.com>
2025-08-11 14:24:33 +1000

    Try adding linting and styling

--- specs/example.spec.ts
+++ specs/example.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Automattic For Agencies', () => {
-  test('has correct title', async ({ page }) => {
-    await page.goto('https://agencies.automattic.com/signup');
-    await expect(page).toHaveTitle('Automattic For Agencies');
-  });
+	test('has correct title', async ({ page }) => {
+		await page.goto('https://agencies.automattic.com/signup');
+		await expect(page).toHaveTitle('Automattic For Agencies');
+	});
 });
