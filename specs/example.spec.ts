90df36cc53b778ceaedc8958a6e722e8f758c1e1
Alister Scott <alister.scott@gmail.com>
2025-08-11 13:24:59 +1000

    MVP

---
+++ specs/example.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Automattic For Agencies', () => {
+  test('has correct title', async ({ page }) => {
+    await page.goto('https://agencies.automattic.com/signup');
+    await expect(page).toHaveTitle('Automattic For Agencies');
+  });
+});
